03cb0e3bda0a1a978edd90f87cf8bdd908789b32
Metehan Toksoy <metehan.toksoy@smartface.com>
2017-04-27 11:13:08 +0300

    Android repository reset

---
+++ Native/Android/src/main/java/io/SmartfaceDemo/A.java
@@ -0,0 +1,14 @@
+package io.smartface.SmartfaceDemo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class A extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
